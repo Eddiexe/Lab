18c84ce1e718156345998bb9bc31ba0eff39173a
domcave <112591064+domcave@users.noreply.github.com>
2023-02-16 09:48:58 -0500

    Create Lab6.py

---
+++ Lab6.py
@@ -0,0 +1,6 @@
+name = input("What is your name?:")
+age = input("What is your age?:")
+if age > 20:
+  print("Hello", name + "! Welcome to our Lab 6 python program! You are so old.")
+else
+    print("Hello", name + "! Welcome to our Lab 6 python program! You are young and energetic.")
